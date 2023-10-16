0aba75f5accc571f76b320ad6fda540725753246
Gabriel Lindo <gabrieldantecandido@gmail.com>
2023-10-16 00:12:33 -0300

    pagina de alertas funcionando

--- app/client/src/pages/Alerts.jsx
+++ app/client/src/pages/Alerts.jsx
@@ -1,27 +1,76 @@
-import React from "react";
-import Alert from "../components/alert"
+// import React from "react";
+// import Alert from "../components/alert"
 
-export default function Verification() {
+// const Verification=()=> {
+//   const [alerts, setAlerts] = React.useState([]);
+
+//   React.useEffect(() => {
+//     // console.log("passou use effect")
+//     fetch("/getAllAlerts")
+//       .then((response) => response.json())
+//       .then((data) => {
+//         setAlerts(data)
+//         console.log(data)
+//       })
+//       .catch(error => console.error('Erro:', error))
+//   }, []);
+
+//   console.log("alerts:" + alerts[0])
+//   return(
+//     <div>
+//       {alerts.map((alert) => {
+//         <h1>{alert.time}</h1>
+//       })}
+//     </div>
+//   )
+
+// }
+// export default Verification
+
+import React, { useState, useEffect } from 'react';
+
+const Verification = () => {
   const [alerts, setAlerts] = React.useState([]);
+  const [bigImage, setBigImage] = React.useState(null);
 
-  React.useEffect(() => {
-    // console.log("passou use effect")
-    fetch("/getAllAlerts")
-      .then((response) => response.json())
-      .then((data) => {
-        setAlerts(data);
-        console.log(data)
-      });
+  useEffect(() => {
+    fetch('/getAllAlerts')
+      .then(response => response.json())
+      .then(data => setAlerts(data))
+      .catch(error => console.error('Erro:', error));
   }, []);
 
-  console.log("alerts:" + alerts)
-  return(
+  return (
+    // <table>
+    //   <thead>
+    //     <tr>
+    //       <th>Hora</th>
+    //       <th>Data</th>
+    //     </tr>
+    //   </thead>
+    //   <tbody>
+    //     {alerts.map((alert) => (
+    //       <tr>
+    //         <td>{alert.time}</td>
+    //         <td>{alert.date}</td>
+    //       </tr>
+    //     ))}
+    //   </tbody>
+    // </table>
     <div>
-      {alerts.map(alert => {
-        <h1>alert.location</h1>
-      })}
+      <h1>Lista de alertas</h1>
+      <img src={bigImage} />
+        {alerts.map((alert, index) => (
+        <div key={index}>
+          <h1>{alert.time}</h1>
+          <h1>{alert.date}</h1>
+          <h1>{alert.location}</h1>
+          <img onClick={()=>{setBigImage(require(`../images/alertsImages/${alert.imageName}`))}} src={require(`../images/alertsImages/${alert.imageName}`)} />
+        </div>
+      ))}
     </div>
-  )
+  );
+};
 
-}
+export default Verification;
 
